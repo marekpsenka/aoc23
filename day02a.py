afef3c4e8deced0f8022ca1f33d8fdaf13bac3fb
Marek Pšenka <psenka@edhouse.cz>
2023-12-02 09:03:42 +0100

    Day 02

--- day02a.py
+++ day02a.py
@@ -1,6 +1,36 @@
 import fileinput
+from operator import itemgetter
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+max_red = 12
+max_green = 13
+max_blue = 14
+
+
+def is_comp_possible(comp_str: str) -> bool:
+    num_str, kind_str = comp_str.split(" ")
+    match kind_str:
+        case "red":
+            return int(num_str) <= max_red
+        case "green":
+            return int(num_str) <= max_green
+        case "blue":
+            return int(num_str) <= max_blue
+        case _:
+            raise RuntimeError
+
+
+def is_draw_possible(draw_str: str) -> bool:
+    comp_strs = draw_str.split(", ")
+    return all(map(is_comp_possible, comp_strs))
+
+
+def analyze_game(line: str) -> tuple[int, bool]:
+    game_part, draws_part = line.split(": ")
+    _, id_part = game_part.split(" ")
+    draw_strs = draws_part.split("; ")
+    return (int(id_part), all(map(is_draw_possible, draw_strs)))
+
+
+print(sum(map(itemgetter(0), filter(itemgetter(1), map(analyze_game, input)))))
--- day02b.py
+++ day02b.py
@@ -1,6 +1,37 @@
 import fileinput
+from functools import reduce
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+Rgb = tuple[int, int, int]
+
+
+def componentwise_max(t1: Rgb, t2: Rgb) -> Rgb:
+    return (max(t1[0], t2[0]), max(t1[1], t2[1]), max(t1[2], t2[2]))
+
+
+def parse_rgb(comp_str: str) -> Rgb:
+    num_str, kind_str = comp_str.split(" ")
+    match kind_str:
+        case "red":
+            return (int(num_str), 0, 0)
+        case "green":
+            return (0, int(num_str), 0)
+        case "blue":
+            return (0, 0, int(num_str))
+
+
+def max_over_draw(draw_str: str) -> Rgb:
+    comp_strs = draw_str.split(", ")
+    return reduce(componentwise_max, map(parse_rgb, comp_strs))
+
+
+def power_of_game(line: str) -> Rgb:
+    game_part, draws_part = line.split(": ")
+    _, id_part = game_part.split(" ")
+    draw_strs = draws_part.split("; ")
+    x = reduce(componentwise_max, map(max_over_draw, draw_strs))
+    return x[0] * x[1] * x[2]
+
+
+print(sum(map(power_of_game, input)))
