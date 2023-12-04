b1f0982891fb3687ff432bfa1d59b0370c2ea6b6
Marek Pšenka <psenka@edhouse.cz>
2023-12-04 07:58:18 +0100

    Day 04

--- day04a.py
+++ day04a.py
@@ -2,5 +2,22 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
+total = 0
+
+
+def parse_set(s: str) -> set[int]:
+    return set(map(int, s.split()))
+
+
 for line in input:
-    print(line)
+    _, numbers_part = line.split(": ")
+    winning_part, mine_part = numbers_part.split(" | ")
+    num_winning = len(
+        parse_set(winning_part).intersection(parse_set(mine_part))
+    )
+
+    if num_winning > 0:
+        total += pow(2, num_winning - 1)
+
+
+print(total)
--- day04b.py
+++ day04b.py
@@ -1,6 +1,20 @@
 import fileinput
 
-input = map(str.rstrip, fileinput.input())
+input = list(map(str.rstrip, fileinput.input()))
+num_cards = [1 for _ in input]
 
-for line in input:
-    print(line)
+
+def parse_set(s: str) -> set[int]:
+    return set(map(int, s.split()))
+
+
+for i, line in enumerate(input):
+    _, numbers_part = line.split(": ")
+    winning_part, mine_part = numbers_part.split(" | ")
+    num_winning = len(
+        parse_set(winning_part).intersection(parse_set(mine_part))
+    )
+    for j in range(i + 1, min(len(input), i + 1 + num_winning)):
+        num_cards[j] += num_cards[i]
+
+print(sum(num_cards))
