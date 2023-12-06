bb966c986c89a99961be3bdcb1e8ee0cf83e338c
Marek Pšenka <psenka@edhouse.cz>
2023-12-06 08:31:52 +0100

    Day 06

--- day06b.py
+++ day06b.py
@@ -1,6 +1,42 @@
 import fileinput
+from math import sqrt, floor, ceil
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+_, times_part = next(input).split(":")
+_, distances_part = next(input).split(":")
+
+time = int("".join(times_part.split()))
+distance = int("".join(distances_part.split()))
+
+
+def nearest_larger_integer(x: float) -> int:
+    c = ceil(x)
+    if float(c) > x:
+        return c
+    else:
+        return c + 1
+
+
+def nearest_smaller_integer(x: float) -> int:
+    f = floor(x)
+    if float(f) < x:
+        return f
+    else:
+        return f - 1
+
+
+def possible_held_time(time: int, distance: int) -> tuple[int, int]:
+    sqrt_discriminant = sqrt(time * time - 4 * distance)
+    return (
+        nearest_larger_integer((time - sqrt_discriminant) / 2.0),
+        nearest_smaller_integer((time + sqrt_discriminant) / 2.0),
+    )
+
+
+def num_possibilities(time: int, distance: int) -> int:
+    pt = possible_held_time(time, distance)
+    return pt[1] - pt[0] + 1
+
+
+print(num_possibilities(time, distance))
