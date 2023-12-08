7766f77583c73863c39062f612e6b6786a7f2df5
Marek Pšenka <psenka@edhouse.cz>
2023-12-08 08:21:53 +0100

    Day 08

--- day08a.py
+++ day08a.py
@@ -1,6 +1,33 @@
 import fileinput
+from itertools import cycle
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+instructions = next(input)
+_ = next(input)
+
+
+def parse_node(line: str) -> tuple[str, tuple[str, str]]:
+    here, dests_part = line.split(" = ")
+    left, right = dests_part[1 : (len(dests_part) - 1)].split(", ")
+    return (here, (left, right))
+
+
+network = dict(map(parse_node, input))
+
+length = 0
+current_node = "AAA"
+instruction_cycle = cycle(instructions)
+
+while current_node != "ZZZ":
+    match next(instruction_cycle):
+        case "L":
+            current_node = network[current_node][0]
+        case "R":
+            current_node = network[current_node][1]
+        case _:
+            raise RuntimeError
+
+    length += 1
+
+print(length)
--- day08b.py
+++ day08b.py
@@ -1,6 +1,42 @@
 import fileinput
+from itertools import cycle
+from math import lcm
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+instructions = next(input)
+_ = next(input)
+
+
+def parse_node(line: str) -> tuple[str, tuple[str, str]]:
+    here, dests_part = line.split(" = ")
+    left, right = dests_part[1 : (len(dests_part) - 1)].split(", ")
+    return (here, (left, right))
+
+
+network = dict(map(parse_node, input))
+
+
+current_nodes = list(filter(lambda s: s[-1] == "A", network.keys()))
+
+
+def traverse(current: str) -> int:
+    length = 0
+    instruction_cycle = cycle(instructions)
+
+    while current[-1] != "Z":
+        match next(instruction_cycle):
+            case "L":
+                current = network[current][0]
+            case "R":
+                current = network[current][1]
+            case _:
+                raise RuntimeError
+
+        length += 1
+
+    return length
+
+
+ls = map(traverse, current_nodes)
+print(lcm(*ls))
