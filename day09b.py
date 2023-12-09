924c0ac788c5b7bf55075ff4417e5281d81c27b5
Marek Pšenka <psenka@edhouse.cz>
2023-12-09 09:24:52 +0100

    Day 09

--- day09b.py
+++ day09b.py
@@ -1,6 +1,22 @@
 import fileinput
+from itertools import pairwise
+
+
+def differences(seq: list[int]) -> list[int]:
+    return list(map(lambda p: p[1] - p[0], pairwise(seq)))
+
+
+def extrapolate(seq: list[int]) -> int:
+    if all(map(lambda n: n == 0, seq)):
+        return 0
+    else:
+        return seq[0] - extrapolate(differences(seq))
+
+
+def line_to_seq(line: str) -> list[int]:
+    return list(map(int, line.split()))
+
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+print(sum(map(extrapolate, map(line_to_seq, input))))
