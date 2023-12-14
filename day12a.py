6ba99d7d2d9614827aaccb7e933d87a9491370af
Marek Pšenka <psenka@edhouse.cz>
2023-12-14 19:17:04 +0100

    Day 12

--- day12a.py
+++ day12a.py
@@ -1,6 +1,59 @@
 import fileinput
+from more_itertools import run_length
+from operator import itemgetter
+from copy import copy
 
-input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+def parse_line(line: str) -> tuple[str, list[int]]:
+    record_part, signature_part = line.split()
+    return (record_part, list(map(int, signature_part.split(","))))
+
+
+records_signatures = list(
+    map(lambda ii: parse_line(str.rstrip(ii)), fileinput.input())
+)
+
+
+def consistent(list1: list[int], list2: list[int]) -> bool:
+    if len(list1) > len(list2):
+        return False
+    else:
+        return all(map(lambda p: p[0] <= p[1], zip(list1, list2)))
+
+
+def signature(s: str) -> list[int]:
+    return list(
+        map(itemgetter(1), filter(lambda p: p[0] == "#", run_length.encode(s)))
+    )
+
+
+def str_replace_at(s: str, i: int, c: str) -> str:
+    chars = list(s)
+    chars[i] = c
+    return "".join(chars)
+
+
+#          ???.### 1,1,3
+def replace_next(s: str, target_signature: list[int]) -> int:
+    pos = s.find("?")
+    if pos < 0:
+        sig = signature(s)
+        if sig == target_signature:
+            return 1
+        else:
+            return 0
+    else:
+        sig = signature(s[:pos])
+        if consistent(sig, target_signature):
+            return replace_next(
+                str_replace_at(copy(s), pos, "."), target_signature
+            ) + replace_next(
+                str_replace_at(copy(s), pos, "#"), target_signature
+            )
+        else:
+            return 0
+
+
+print(replace_next(*records_signatures[2]))
+
+print(sum(map(lambda p: replace_next(*p), records_signatures)))
--- day12b.py
+++ day12b.py
@@ -1,6 +1,39 @@
 import fileinput
+from functools import cache
 
-input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+def parse_line(line: str) -> tuple[str, tuple[int, ...]]:
+    record_part, signature_part = line.split()
+    return (
+        "?".join([record_part] * 5) + ".",
+        tuple(map(int, signature_part.split(","))) * 5,
+    )
+
+
+records_signatures = list(
+    map(lambda ii: parse_line(str.rstrip(ii)), fileinput.input())
+)
+
+
+@cache
+def count_arrangements(s: str, sig: tuple[int, ...]) -> int:
+    if len(sig) == 0:
+        if "#" in s:
+            return 0
+        else:
+            return 1
+
+    n = sig[0]
+    count = 0
+    for i in range(len(s) - sum(sig) - (len(sig) - 1)):
+        if s[i + n] == "#":
+            continue
+        if "#" in s[:i]:
+            break
+        if "." not in s[i : (i + n)]:
+            count += count_arrangements(s[(i + n + 1) :], sig[1:])
+
+    return count
+
+
+print(sum(map(lambda p: count_arrangements(*p), records_signatures)))
