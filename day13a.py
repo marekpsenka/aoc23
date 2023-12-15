0b1a9686696ceec33a3fc6c4d5f490152b4c25d1
Marek Pšenka <psenka@edhouse.cz>
2023-12-15 07:58:24 +0100

    Day 13

--- day13a.py
+++ day13a.py
@@ -1,6 +1,36 @@
 import fileinput
+from more_itertools import split_at, transpose
+from typing import Optional
 
-input = map(str.rstrip, fileinput.input())
+input = map(lambda line: tuple(str.rstrip(line)), fileinput.input())
 
-for line in input:
-    print(line)
+patterns = split_at(input, lambda line: not line)
+
+sum_of_column_numbers_to_the_left = 0
+sum_of_row_counts_above = 0
+
+
+def find_axis(rs: list[tuple[str, ...]]) -> Optional[int]:
+    for i in range(len(rs) - 1):
+        if all(
+            map(
+                lambda p: p[0] == p[1],
+                zip(reversed(rs[: (i + 1)]), rs[(i + 1) :]),
+            )
+        ):
+            return i
+    return None
+
+
+for pattern in patterns:
+    axis = find_axis(pattern)
+    if axis is not None:
+        sum_of_row_counts_above += axis + 1
+    else:
+        axis = find_axis(list(transpose(pattern)))
+        if axis is not None:
+            sum_of_column_numbers_to_the_left += axis + 1
+        else:
+            raise RuntimeError
+
+print(sum_of_column_numbers_to_the_left + 100 * sum_of_row_counts_above)
--- day13b.py
+++ day13b.py
@@ -1,6 +1,56 @@
 import fileinput
+from more_itertools import split_at
+from typing import Optional
+from operator import itemgetter
 
-input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+def transpose(li: list[list[bool]]) -> list[list[bool]]:
+    return list(map(list, zip(*li)))
+
+
+def conv(c: str) -> bool:
+    if c == "#":
+        return True
+    else:
+        return False
+
+
+input = map(lambda line: list(map(conv, str.rstrip(line))), fileinput.input())
+
+patterns = list(split_at(input, lambda line: not line))
+
+
+def diff(s1: list[bool], s2: list[bool]) -> list[int]:
+    return list(
+        map(
+            itemgetter(0),
+            filter(lambda p: p[1][0] != p[1][1], enumerate(zip(s1, s2))),
+        )
+    )
+
+
+def find_axis_with_one_smudge(rs: list[list[bool]]) -> Optional[int]:
+    for i in range(len(rs) - 1):
+        diffs: list[tuple[int, int]] = []
+        for j in range(min(i + 1, len(rs) - i - 1)):
+            diffs += map(lambda k: (i - j, k), diff(rs[i - j], rs[i + j + 1]))
+
+        if len(diffs) == 1:
+            return i
+
+    return None
+
+
+sum_of_column_numbers_to_the_left = 0
+sum_of_row_counts_above = 0
+
+for pattern in patterns:
+    axis = find_axis_with_one_smudge(pattern)
+    if axis is not None:
+        sum_of_row_counts_above += axis + 1
+    else:
+        axis = find_axis_with_one_smudge(list(transpose(pattern)))
+        if axis is not None:
+            sum_of_column_numbers_to_the_left += axis + 1
+
+print(sum_of_column_numbers_to_the_left + 100 * sum_of_row_counts_above)
